07a3fe66d3825f43f39bd770d0ed02a1af091ea0
Marat <148357498+maryik@users.noreply.github.com>
2024-09-22 19:31:08 +0300

    feat: adding images to storage, downloading library multer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 
 import {registerValidation, loginValidation, postCreateValidation   } from "./validations.js";
 
@@ -15,18 +16,34 @@ mongoose.connect(
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({storage});
+
 app.use(express.json());
 
 app.post("/auth/login", loginValidation, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
 
+app.post("/upload", checkAuth, upload.single('image'), (req, res)=>{
+    res.json({
+        url: '/uploads/${req.file.originalname}',
+    });
+});
 
-app.get("/posts", PostController.getAll)
-app.get("/posts/:id", PostController.getOne)
-app.post("/posts", checkAuth, postCreateValidation, PostController.create)
-app.delete("/posts/:id", checkAuth, PostController.remove)
-app.patch("/posts/:id", checkAuth, PostController.update)
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id", checkAuth, PostController.update);
 
 app.listen(4444, (err)=>{
     if(err){
