3b97164473592983de4ffe7550d9b857ca413488
Marat <148357498+maryik@users.noreply.github.com>
2024-09-22 19:55:03 +0300

    fix: moving validation to a separate utility and adding in varios routes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import multer from "multer";
 
 import {registerValidation, loginValidation, postCreateValidation   } from "./validations.js";
-
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UserController   from './controllers/UserController.js'
@@ -30,8 +30,8 @@ const upload = multer({storage});
 app.use(express.json());
 app.use("/uploads", express.static('uploads'));
 
-app.post("/auth/login", loginValidation, UserController.login);
-app.post("/auth/register", registerValidation, UserController.register);
+app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
+app.post("/auth/register", registerValidation, handleValidationErrors, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.post("/upload", checkAuth, upload.single('image'), (req, res)=>{
@@ -42,9 +42,9 @@ app.post("/upload", checkAuth, upload.single('image'), (req, res)=>{
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
-app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.post("/posts", checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete("/posts/:id", checkAuth, PostController.remove);
-app.patch("/posts/:id", checkAuth, PostController.update);
+app.patch("/posts/:id", checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
 app.listen(4444, (err)=>{
     if(err){
